d152b0470a9e00c0e9130b5e4a544d5a31de4f6a
kcapiga <01187312@pw.edu.pl>
2024-06-10 10:05:55 +0200

    Poprawienie wzorów

--- plotter.py
+++ plotter.py
@@ -20,18 +20,24 @@ if not  len(acc_array) == len(gyr_array):
         dl = len(gyr_array) - len(acc_array)
         gyr_array = gyr_array[0:len(gyr_array) - dl]
 dt = (acc_data[-1]["time"] / len(acc_data))
-print(dt)
+#print(dt)
 acc = []
 for i in range(len(acc_array)):
     acc.append(q_rot(q_conj(gyr_array[i]), acc_array[i]))
 acc = np.array(acc)
-
-pos = []
+vel = []
 for i in range(len(acc)):
     if (i == 0):
-        pos.append([acc[i][0]*i*dt*dt*0.5, acc[i][1]*i*dt*dt*0.5, acc[i][2]*i*dt*dt*0.5])
+        vel.append([acc[i][0]*dt, acc[i][1]*dt, acc[i][2]*dt])
     else:
-        pos.append([pos[i-1][0] + acc[i][0]*dt*i*dt*0.5, pos[i-1][1] + acc[i][1]*dt*i*dt*0.5, pos[i-1][2] + acc[i][2]*i*dt*dt*0.5])
+        vel.append([acc[i-1][0] + acc[i][0]*dt, acc[i-1][1] + acc[i][1]*dt, acc[i-1][2] + acc[i][2]*dt])
+vel = np.array(vel)
+pos = []
+for i in range(len(vel)):
+    if (i == 0):
+        pos.append([acc[i][0]*dt*dt*0.5, acc[i][1]*dt*dt*0.5, acc[i][2]*dt*dt*0.5])
+    else:
+        pos.append([pos[i-1][0] + vel[i-1][0]*dt + acc[i][0]*dt*dt*0.5, pos[i-1][1] + vel[i][1]*dt + acc[i][1]*dt*dt*0.5, pos[i-1][2] + vel[i][2]*dt + acc[i][2]*dt*dt*0.5])
 pos = np.array(pos)
 
 fig = plt.figure()
@@ -41,7 +47,7 @@ ax.set_xlabel('X Position')
 ax.set_ylabel('Y Position')
 ax.set_zlabel('Z Position')
 ax.legend()
-ax.set_xlim(-10, 10)
-ax.set_ylim(-10, 10)
-ax.set_zlim(0, 10)
+ax.set_xlim(-2, 2)
+ax.set_ylim(-2, 2)
+ax.set_zlim(-2, 2)
 plt.show()
