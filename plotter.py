dcc8f3513375ebcb75643167ab583931913919ef
kcapiga <01187312@pw.edu.pl>
2024-06-20 16:53:16 +0200

    Poprawienie równań

--- plotter.py
+++ plotter.py
@@ -4,9 +4,9 @@ from ahrs.common.orientation import q_conj, q_rot
 import matplotlib.pyplot as plt
 
 #otwarcie zapisanej w JSON-ie trasy
-with open('resource/kolo/acc.json', 'r') as file:
+with open('resource/acc.json', 'r') as file:
     acc_data = json.load(file)
-with open('resource/kolo/gyr.json', 'r') as file:
+with open('resource/gyr.json', 'r') as file:
     gyr_data = json.load(file)
 #ahrs = imufusion.Ahrs() #przydatne rzeczy
 acc_array = np.array([[d["x"], d["y"], d["z"]] for d in acc_data])
@@ -24,20 +24,21 @@ acc = []
 for i in range(len(acc_array)):
     acc.append(q_rot(q_conj(gyr_array[i]), acc_array[i]))
 acc = np.array(acc)
+
 vel = []
 for i in range(len(acc)):
     if (i == 0):
-        vel.append([acc[i][0]*dt, acc[i][1]*dt, acc[i][2]*dt])
+        vel.append([0, 0, 0])
     else:
-        vel.append([acc[i-1][0] + acc[i][0]*dt, acc[i-1][1] + acc[i][1]*dt, acc[i-1][2] + acc[i][2]*dt])
+        vel.append([vel[i-1][0] + acc[i][0]*dt, vel[i-1][1] + acc[i][1]*dt, vel[i-1][2] + acc[i][2]*dt])
 vel = np.array(vel)
 pos = []
 for i in range(len(vel)):
     if (i == 0):
-        pos.append([acc[i][0]*dt*dt*0.5, acc[i][1]*dt*dt*0.5, acc[i][2]*dt*dt*0.5])
+        pos.append([0, 0, 0])
     else:
-        pos.append([pos[i-1][0] + vel[i-1][0]*dt + acc[i-1][0]*dt*dt*0.5, pos[i-1][1] + vel[i-1][1]*dt +
-                    acc[i-1][1]*dt*dt*0.5, pos[i-1][2] + vel[i-1][2]*dt + acc[i-1][2]*dt*dt*0.5])
+        pos.append([pos[i-1][0] + vel[i][0]*dt + 0.5*acc[i][0]*dt**2, pos[i-1][1] + vel[i][1]*dt +
+                    0.5*acc[i][1]*dt**2, pos[i-1][2] + vel[i][2]*dt + 0.5*acc[i][2]*dt**2])
 pos = np.array(pos)
 
 fig = plt.figure()
@@ -47,7 +48,8 @@ ax.set_xlabel('X Position')
 ax.set_ylabel('Y Position')
 ax.set_zlabel('Z Position')
 ax.legend()
-ax.set_xlim(-2, 2)
-ax.set_ylim(-2, 2)
-ax.set_zlim(-2, 2)
+width = 1
+ax.set_xlim(-width, width)
+ax.set_ylim(-width, width)
+ax.set_zlim(-width, width)
 plt.show()
