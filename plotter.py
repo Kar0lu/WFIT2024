470426445a281adbef7fa200b72bb205f2d3112c
Kar0lu <karolugod@gmail.com>
2024-06-20 17:16:05 +0200

    Final version

--- plotter.py
+++ plotter.py
@@ -3,15 +3,15 @@ import numpy as np
 from ahrs.common.orientation import q_conj, q_rot
 import matplotlib.pyplot as plt
 
-#otwarcie zapisanej w JSON-ie trasy
-with open('resource/acc.json', 'r') as file:
+# Open JSON files with trail data
+with open('resource/kolo/acc.json', 'r') as file:
     acc_data = json.load(file)
-with open('resource/gyr.json', 'r') as file:
+with open('resource/kolo/gyr.json', 'r') as file:
     gyr_data = json.load(file)
-#ahrs = imufusion.Ahrs() #przydatne rzeczy
 acc_array = np.array([[d["x"], d["y"], d["z"]] for d in acc_data])
 gyr_array = np.array([[d["qx"], d["qy"], d["qz"], d["qw"]] for d in gyr_data])
 
+# Sample number equalisation
 if not  len(acc_array) == len(gyr_array):
     if len(acc_array) > len(gyr_array):
         dl = len(acc_array) - len(gyr_array)
@@ -20,11 +20,14 @@ if not  len(acc_array) == len(gyr_array):
         dl = len(gyr_array) - len(acc_array)
         gyr_array = gyr_array[0:len(gyr_array) - dl]
 dt = (acc_data[-1]["time"] / len(acc_data))
+
+# Rotating acceleration vector using quaternion
 acc = []
 for i in range(len(acc_array)):
     acc.append(q_rot(q_conj(gyr_array[i]), acc_array[i]))
 acc = np.array(acc)
 
+# Integrating acceleration to recive velocity
 vel = []
 for i in range(len(acc)):
     if (i == 0):
@@ -32,6 +35,8 @@ for i in range(len(acc)):
     else:
         vel.append([vel[i-1][0] + acc[i][0]*dt, vel[i-1][1] + acc[i][1]*dt, vel[i-1][2] + acc[i][2]*dt])
 vel = np.array(vel)
+
+# Integrating velocity to recive position
 pos = []
 for i in range(len(vel)):
     if (i == 0):
@@ -41,6 +46,7 @@ for i in range(len(vel)):
                     0.5*acc[i][1]*dt**2, pos[i-1][2] + vel[i][2]*dt + 0.5*acc[i][2]*dt**2])
 pos = np.array(pos)
 
+# Plotting
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 ax.plot(pos[:, 0], pos[:, 1], pos[:, 2], label='Sensor Trajectory')
@@ -48,7 +54,8 @@ ax.set_xlabel('X Position')
 ax.set_ylabel('Y Position')
 ax.set_zlabel('Z Position')
 ax.legend()
-width = 1
+width = 5
+width = width/2
 ax.set_xlim(-width, width)
 ax.set_ylim(-width, width)
 ax.set_zlim(-width, width)
--- server.py
+++ server.py
@@ -13,6 +13,7 @@ app = Flask(__name__)
 acc_data = []
 gyr_data = []
 
+# Method to receive sensor data on server
 @app.route('/data', methods=['POST'])
 def data():
     if request.method == "POST":
@@ -32,6 +33,7 @@ def data():
     else:
         return jsonify({"status": "method not allowed"}), 405
 
+# Method to save sensor data to json file
 @app.route('/get', methods=["GET"])
 def get():
     acc_data[0]["time"] = 0
@@ -44,132 +46,6 @@ def get():
         acc_data.clear()
         gyr_data.clear()
         return jsonify({"status": "success"}), 200
-'''
-@app.route('/plot', methods=["GET"])
-def plot():
-    if request.method == "GET":
-        # Read data from acc.json and gyr.json
-        with open('acc.json', 'r') as file:
-            acc_data = json.load(file)
-        with open('gyr.json', 'r') as file:
-            gyr_data = json.load(file)
-        
-        # acc_array = np.array([[d["time"], d["x"], d["y"], d["z"]] for d in acc_data])
-        # gyr_array = np.array([[d["time"], d["x"], d["y"], d["z"]] for d in gyr_data])
-        
-        # plot_acceleration(acc_array)
-        # plot_gyroscope(gyr_array)
-        
-        pos = calculate_position(acc_data, gyr_data)
-        # plot_trajectory(pos)
-        # return send_file('trajectory.png', mimetype='image/png')
-        return "xd"
-
-def calculate_position(acc_data, gyr_data):
-
-    quat  = np.zeros((len(acc_data), 4), dtype=np.float64)
-    dt = acc_data[-1]["time"] / len(acc_data)
-    time = [t*dt for t in range(0, len(acc_data))]
-
-    acc = []
-    gyr = []
-
-    # gyr = np.zeros(3, dtype=np.float64)
-
-    # for t in range(0, len(acc_data)):
-    #     acc = np.array([[acc_data[t]["x"], acc_data[t]["y"], acc_data[t]["z"]]])
-    # acc = acc.mean(axis=0)
-    
-    mahony = ahrs.filters.Mahony(Kp=1, Ki=0,KpInit=1, frequency=1/dt)
-    # q = np.array([1.0,0.0,0.0,0.0], dtype=np.float64)
-    q = np.tile([1., 0., 0., 0.], (len(acc_data), 1)) 
-    # print(q)
-    # for i in range(0, len(acc_data)):
-    #     q = mahony.updateIMU(q, gyr=gyr, acc=acc)
-    
-    for t in range(0, len(acc_data)):
-        acc.append(np.array([acc_data[t]["x"], acc_data[t]["y"], acc_data[t]["z"]]))
-        gyr.append(np.array([gyr_data[t]["x"], gyr_data[t]["y"], gyr_data[t]["z"]])*np.pi/180)
-
-    for t in range(1, len(acc_data)):
-        # print(q[t-1],gyr[t],acc[t])
-        q[t]=mahony.updateIMU(q[t-1],gyr=gyr[t],acc=acc[t])
-
-    acc = []
-    for i in range(0, len(acc_data)):
-        acc.append(q_rot(q_conj(q[i]), np.array([acc_data[i]['x'], acc_data[i]['y'], acc_data[i]['z']])))
-    acc = np.array(acc)
-    acc = acc - np.array([0,0,0.0000981])
-    
-    vel = np.zeros(acc.shape)
-    for t in range(1,vel.shape[0]):
-        vel[t,:] = vel[t-1,:] + acc[t,:]*dt
-    
-    pos = np.zeros(vel.shape)
-    for t in range(1,pos.shape[0]):
-        pos[t,:] = pos[t-1,:] + vel[t,:]*dt
-    
-    posPlot = pos
-    quatPlot = quat
-
-    print(pos)
-    print(quat)
-
-    extraTime = 20
-    onesVector = np.ones(int(extraTime*(1/dt)))
-
-    # Create 6 DOF animation
-    fig = plt.figure(figsize=(7, 7))
-    ax = fig.add_subplot(111, projection='3d') # Axe3D object
-    ax.plot(posPlot[:,0],posPlot[:,1],posPlot[:,2])
-    # min_, max_ = np.min(np.min(posPlot,axis=0)), np.max(np.max(posPlot,axis=0))
-    # ax.set_xlim(-0.01,0.01)
-    # ax.set_ylim(-0.01,0.01)
-    ax.set_zlim(-0.1,0.1)
-    ax.set_title("trajectory")
-    ax.set_xlabel("x position (m)")
-    ax.set_ylabel("y position (m)")
-    ax.set_zlabel("z position (m)")
-    
-    plt.show()
-
-
-def plot_acceleration(acc_data):
-    times = acc_data[:, 0] * 1e-9  # Convert time from nanoseconds to seconds
-    plt.figure()
-    plt.plot(times, acc_data[:, 1], label='X Acceleration')
-    plt.plot(times, acc_data[:, 2], label='Y Acceleration')
-    plt.plot(times, acc_data[:, 3], label='Z Acceleration')
-    plt.xlabel('Time (s)')
-    plt.ylabel('Acceleration (m/s^2)')
-    plt.title('Acceleration Over Time')
-    plt.legend()
-    plt.savefig('acceleration.png')
-
-def plot_gyroscope(gyr_data):
-    times = gyr_data[:, 0] * 1e-9  # Convert time from nanoseconds to seconds
-    plt.figure()
-    plt.plot(times, gyr_data[:, 1], label='X Angular Rate')
-    plt.plot(times, gyr_data[:, 2], label='Y Angular Rate')
-    plt.plot(times, gyr_data[:, 3], label='Z Angular Rate')
-    plt.xlabel('Time (s)')
-    plt.ylabel('Angular Rate (rad/s)')
-    plt.title('Gyroscope Data Over Time')
-    plt.legend()
-    plt.savefig('gyroscope.png')
-
-def plot_trajectory(position):
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    
-    ax.plot(position[:, 0], position[:, 1], position[:, 2], label='Sensor Trajectory')
-    ax.set_xlabel('X Position')
-    ax.set_ylabel('Y Position')
-    ax.set_zlabel('Z Position')
-    ax.legend()
-    
-    plt.savefig('trajectory.png')
-'''
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host="0.0.0.0")
